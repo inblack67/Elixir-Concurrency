e4067fec6204814387329c836bc96620658a5199
ryuzaki <ryuzaki>
2021-07-11 12:10:07 +0530

    supervisor

--- lib/chat.ex
+++ lib/chat.ex
@@ -2,20 +2,21 @@ defmodule Concurrency.Chat do
   use GenServer
 
   # client side
-  def start_link do
+  def start_link(_pid) do
     # start a process with the attributes of the current module
-    GenServer.start_link(__MODULE__, [])
+    # downside to using :chat_room atom rather than process id is that only one process can be assigned now (:chat_room)
+    GenServer.start_link(__MODULE__, [], name: :chat_room)
   end
 
-  def get_messages(pid) do
+  def get_messages do
     # blocking call => if handle_call callback is not available => this will stop running the code
     # synch request
-    GenServer.call(pid, :get_messages)
+    GenServer.call(:chat_room, :get_messages)
   end
 
-  def post_message(pid, message) do
+  def post_message(message) do
     # asynch request
-    GenServer.cast(pid, {:post_message, message})
+    GenServer.cast(:chat_room, {:post_message, message})
   end
 
   # server side / callback functions
@@ -33,3 +34,6 @@ defmodule Concurrency.Chat do
     {:noreply, [message | messages]}
   end
 end
+
+# iex(5)> Process.whereis(:chat_room)
+# iex(6)> Process.whereis(:chat_room) |> Process.exit(:kill) # will be restarted again with new process id by the supervisor
--- lib/superviser.ex
+++ lib/superviser.ex
@@ -1,4 +1,24 @@
 defmodule Concurrency.Supervisor do
+  use Supervisor
+
+  alias Concurrency.Chat
+
   # supervisor allows us to create processes which watch are processes and if a process dies, supervisor will try to restart it
-  
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true
+  def init(_) do
+    # if one of our processes dies => respawns
+    # :one_for_all => if one dies => restart all
+    # :rest_for_one => restart it and any process activated after it
+
+    children = [
+      {Chat, []}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 end
